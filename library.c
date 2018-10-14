87adf9f6e2beaefa93a579a93fac10962791beac
wzhou2 <wzhou2@stuy.edu>
2018-10-14 18:55:54 -0400

    added library headers

---
+++ library.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+struct song_node * table[27];
+
+void print_library();
+
+void print_letter(char *);
+
+struct song_node * find(char *, char *);
+
+struct song_node * find_artist(char *);
+
+void remove_song(struct song_node *);
+
+void clear_library();
+
+void add_song(struct song_node *);
+
+void print_artist(char *);
+
+void shuffle(); 
