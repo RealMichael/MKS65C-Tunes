e6d124d15bd93759bd05df731fcdd53e02782705
Michael Luo <33794837+RealMichael@users.noreply.github.com>
2018-10-14 23:38:19 -0400

    insert front works, printing prints song name first instead of artist, reverse order when artists have same name

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+#include "linkedList.h"
+#include <stdlib.h>
 
-int main() {
-    
-    return 0;
+int main(){
+	struct song_node * test;
+
+	//printf("testing insertion in the front");
+	test = insert_front(test,"Wang","Understand the Concepts");
+	print_list(test);
+
+	//printf("testing alphabetical ordering!!);
+	test = insert(test,"Wang","Don't understand the Concepts");
+	test = insert(test,"Apple", "Keeps the doc Away");
+	test = insert(test,"Wang", "Apples are no good");
+	print_list(test);
+	return 0;
 }
