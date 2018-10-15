5e4f36ad220a8901f295998e90debf9f25c9a401
wzhou2 <wzhou2@stuy.edu>
2018-10-15 15:36:01 -0400

    began testing. ->insert and print

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "linkedList.h"
+
+int main() {
+  struct song_node *l = NULL;
+
+  printf("TESTING INSERT_FRONT/INSERT/PRINT_LIST\n");
+  l = insert_front(l, "THE BIG L", "HEY");
+  print_list(l);
+  l = insert(l, "THE BIG L", "ABC");
+  print_list(l);
+  l = insert(l, "THE BIG L", "CAT");
+  print_list(l);
+  l = insert(l, "THE BIG L", "ZZZ");
+  print_list(l);
+
+  printf("TESTING PRINT NODE\n");
+  print_node(l);
+
+  return 0;
+}
