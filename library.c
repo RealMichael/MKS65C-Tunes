d0fbf40622a0ba66bc62cc1d975b965e6baf2836
Michael Luo <33794837+RealMichael@users.noreply.github.com>
2018-10-16 01:18:24 -0400

    wrote find, clear, and add without testing

--- library.c
+++ library.c
@@ -22,15 +22,34 @@ void print_letter(char * let){
 	
 	
 
-struct song_node * find(char *, char *);
+struct song_node * find(char * songName, char * artist);
+	song_node * arr = table;
+	int res = strcmp(artist,"a");
+	song_node * result = find_node(arr + res, songName);
+	return result;
+}
+	
 
 struct song_node * find_artist(char *);
 
 void remove_song(struct song_node *);
 
-void clear_library();
+void clear_library(){
+	song_node arr = table;
+	int i = 0;
+	while(i < 27){
+	free_list(arr + i);
+	i ++;
+	}
+}
 
-void add_song(char*, char*);
+void add_song(char* son, char* art){
+	song_node * arr = table;
+	int res = strcmp(art,"a");
+	insert(arr + res,son,art);
+	}
+
+
 
 void print_artist(char *);
 
