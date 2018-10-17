a2eb11f5db373d9570899afee73aaf3fe2e96231
Michael Luo <33794837+RealMichael@users.noreply.github.com>
2018-10-16 22:10:59 -0400

    added remove, find artist, print artist, missing shuffle

--- library.c
+++ library.c
@@ -17,22 +17,30 @@ void print_library(){
 void print_letter(char * let){
 	song_node * arr = table;
 	int res = strcmp(let,"a");
-	print_list(arr + res);
+	print_list(*(arr + res));
 }
 	
 	
 
-struct song_node * find(char * songName, char * artist);
+struct song_node * find(char * songName, char * artist){
 	song_node * arr = table;
 	int res = strcmp(artist,"a");
-	song_node * result = find_node(arr + res, songName);
+	song_node * result = find_node(find_artist(artist), songName);
 	return result;
 }
 	
 
-struct song_node * find_artist(char *);
+struct song_node * find_artist(char *){
+	song_node * arr = table;
+	int res = strcmp(let,"a");
+	return (*(arr + res));
+	}
 
-void remove_song(struct song_node *);
+void remove_song(char* songName, char * artist){
+	struct song_node res = find(songName,artist);
+	song_node * arr = table;
+	int res2 = strcmp(artist,"a");
+	remove_node(find_artist(artist),res);
 
 void clear_library(){
 	song_node arr = table;
@@ -46,11 +54,34 @@ void clear_library(){
 void add_song(char* son, char* art){
 	song_node * arr = table;
 	int res = strcmp(art,"a");
-	insert(arr + res,son,art);
+	insert(find_artist(son),son,art);
 	}
 
 
 
-void print_artist(char *);
+void print_artist(char * artist){
+	song_node * arr = find_artist(artist);
+	if(arr){
+		while(list -> next){
+		if(list-> artist == artist){
+			print_node(list);
+			list = list -> next;
+		}
+	}
+			if(list -> artist == artist){
+				print_node(list);
+		}
+	}
+	}
+
 
 void shuffle(); 
+
+
+
+
+
+
+
+
+
