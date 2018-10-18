c01cc14811a0fe27c28d0e3bfe7e311ee681aaec
wzhou2 <wzhou2@stuy.edu>
2018-10-17 21:49:11 -0400

    fixed michael's stupid errors

--- library.c
+++ library.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "library.h"
 #include "linkedList.h"
 #include <ctype.h>
@@ -9,25 +10,27 @@ struct song_node * table[27];
 int findArr(char * artist){
 	int arrNum = 26;
 	if((tolower(artist[0])) >= 'a' && (tolower(artist[0])) <= 'z'){
-	arrNum = (tolower(artist[0])) - 97;
-	}
-	else{
-	return arrNum;
+        arrNum = (tolower(artist[0])) - 97;
 	}
 	return arrNum;
-	}
+}
 
 void print_library(){
-	
 	int i = 0;
 	while(i < 27){
-		print_list(table[i]);
-		i ++;
+        if (table[i]) {
+            if (i != 26) {
+                printf("%c list\n", i+65);
+            } else {
+                printf("special characters list\n");
+            }
+            print_list(table[i]);
+        }
+		i++;
 	}
-    }
+}
 
 void print_letter(char * let){
-	
 	int res = findArr(let);
 	print_list(table[res]);
 }
@@ -35,39 +38,39 @@ void print_letter(char * let){
 	
 
 struct song_node * find(char * songName, char * artist){
-	
 	int res = findArr(artist);
 	struct song_node * result = find_node(table[res],songName,artist);
 	return result;
 }
 	
 
-struct song_node * find_artistLib(char * let){
-	
-	int res = findArr(let);
-	return (table[res]);
+struct song_node * find_artistLib(char * artist){
+	int res = findArr(artist);
+	return (find_artist(table[res], artist));
 	}
 
 void remove_song(char* songName, char * artist){
+    printf("AHHH");
 	struct song_node * res = find(songName,artist);
-	
+	printf("HERE?");
 	int res2 = findArr(artist);
-	remove_node(table[res2],res);
+	table[res2] = remove_node(table[res2],res);
 }
 
 void clear_library(){
 	
 	int i = 0;
 	while(i < 27){
-	free_list(table[i]);
-	i ++;
+        if (table[i]) {
+            free_list(table[i]);
+        }
+        i ++;
 	}
 }
 
 void add_song(char* son, char* art){
-	
-	int res = findArr(art);
-	insert(table[res],son,art);
+        int res = findArr(art);
+        table[res] = insert(table[res],son,art);
 	}
 
 
@@ -75,19 +78,14 @@ void add_song(char* son, char* art){
 void print_artist(char * artist){
 	
 	int res = findArr(artist);
-	struct song_node * list = table[res];
-	if(list){
-		while(list -> next){
-		if(list-> artist == artist){
-			print_node(list);
-			list = list -> next;
-		}
-	}
-			if(list -> artist == artist){
-				print_node(list);
-		}
-	}
-	}
+	struct song_node * list = find_artist(table[res], artist);
+	while(list){
+        if(!strcmp(list->artist,artist)){
+            print_node(list);
+        }
+        list = list -> next;
+    }
+}
 
 
 void shuffle(){
